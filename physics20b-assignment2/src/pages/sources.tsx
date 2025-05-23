8cade19c5a737615d91e84a761d675236a5316bf
Rhea Shetti <shettir@uci.edu>
2025-05-23 12:31:57 -0700

    added sources and start deployment

--- physics20b-assignment2/src/pages/sources.tsx
+++ physics20b-assignment2/src/pages/sources.tsx
@@ -5,6 +5,9 @@ const Sources: React.FC = () => {
             <h2> Sources </h2>
             <h4> <a href="https://science.nasa.gov/dark-energy/" target="_blank" rel="noopener noreferrer"> NASA - Dark Energy</a> </h4>
             <h4> <a href="https://news.uchicago.edu/explainer/dark-energy-explained" target="_blank" rel="noopener noreferrer"> University of Chicago - Dark Energy, Explained </a> </h4>
+            <h4> <a href="https://rubinobservatory.org/" target="_blank" rel="noopener noreferrer"> Vera C. Rubin Observatory </a> </h4>
+            <h4> <a href="https://www.esa.int/Science_Exploration/Space_Science/Euclid" target="_blank" rel="noopener noreferrer"> ESA - Euclid Space Telescope </a> </h4>
+            <h4> <a href="https://roman.gsfc.nasa.gov/" target="_blank" rel="noopener noreferrer"> NASA - Nancy Grace Roman Space Telescope </a> </h4>
         </section>
     )
 
